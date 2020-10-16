8e4c90e896e8f9122a3a1efa522859fb24da85dc
brae <brae@braebie.com>
2020-10-15 19:28:34 -0700

    move all hud static menu's into a single file

---
+++ mp/src/game/client/momentum/ui/HUD/hud_menus.cpp
@@ -0,0 +1,42 @@
+#include "cbase.h"
+#include "hud_menu_static.h"
+#include "mom_player_shared.h"
+
+#include "tier0/memdbgon.h"
+
+static void OnRulerMenuClose()
+{
+    engine->ExecuteClientCmd("mom_ruler_close");
+}
+
+static void OnSavelocMenuClose()
+{
+    engine->ExecuteClientCmd("mom_saveloc_close");
+}
+
+static void ShowMenu(const char *filename, void (*ClosFunc)())
+{
+    CHudMenuStatic *pMenu = GET_HUDELEMENT(CHudMenuStatic);
+    if (pMenu && engine->IsInGame())
+    {
+        if (pMenu->IsMenuDisplayed())
+        {
+            pMenu->HideMenu(); // NOTE: if another menu is open this will close it!
+        }
+        else
+        {
+            pMenu->ShowMenu(filename, ClosFunc);
+        }
+    }
+}
+
+CON_COMMAND(showRuler, "Opens the ruler tool.\n")
+{
+    ShowMenu("rulermenu", OnRulerMenuClose);
+}
+
+CON_COMMAND(mom_saveloc_show, "Opens the saved locations menu.\n")
+{
+    ShowMenu("savelocmenu", OnSavelocMenuClose);
+}
+
